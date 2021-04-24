e64abee5b68426cf60b71bdb29572d50a2436fec
Jeronimo Repetto <jeronimorepetto@gmail.com>
2021-04-24 06:23:09 -0300

    Jugabilidad - Partículas - Físicas - GameManager - Controllers

---
+++ 09_ProyectoUdemy_QuickClink/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GameState
+    {
+        loading,
+        inGame,
+        gameOver,
+        pause
+    }
+    [SerializeField]
+    private GameState _gameState;
+    public GameState gameState { get => _gameState; }
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button resetButton;
+    private GameObject followMouseObject;
+    [SerializeField]
+    private List<GameObject> objects;
+    [SerializeField, Range(0.1f, 10f)]
+    private float spawnSeconds = 1f;
+    private float xPosition = 4f;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameState = GameState.inGame;
+        resetButton.gameObject.SetActive(false);
+        gameOverText.gameObject.SetActive(false);
+        UpdateScore(0);
+        StartCoroutine(spawnObjects());
+        followMouseObject = GameObject.Find("FollowMouse");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        followMouseObject.transform.position = MousePosition();
+    }
+
+    /// <summary>
+    /// Encuentra la posicion actual del mouse en la pantalla
+    /// </summary>
+    /// <returns>
+    /// Devuelve un Vector3 con las coordinadas del Mouse en cada frame.
+    /// Posicion Z la settea para que se coloque delante de los objetos
+    /// </returns>
+    private Vector3 MousePosition() 
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = -5f;
+        return mousePosition;
+    }
+
+    /// <summary>
+    /// Genera un Vector3 de forma aleatoria
+    /// </summary>
+    /// <returns>Devuevlve una posicion aleatoria</returns>
+    private Vector3 RandomPosition()
+    {
+        return new Vector3(Random.Range(-xPosition, xPosition), -5f);
+    }
+
+    /// <summary>
+    /// Instancia un objeto aleatorio
+    /// </summary>
+    /// <returns>Devuelve la instancia de un objeto</returns>
+    IEnumerator spawnObjects()
+    {
+        while (_gameState.Equals(GameState.inGame))
+        {
+            int randomNum = Random.Range(0, objects.Count);
+            yield return new WaitForSeconds(spawnSeconds);
+            Instantiate(objects[randomNum], RandomPosition(), objects[randomNum].transform.rotation);
+        }
+
+    }
+
+    /// <summary>
+    /// Actualiza la puntación 
+    /// </summary>
+    /// <param name="point">Número de punto a añadir al score</param>
+    public void UpdateScore(int point) 
+    {
+        score += point;
+        scoreText.text = "Score:\n" + score;
+    }
+
+    /// <summary>
+    /// Finalizacion del juego
+    /// </summary>
+    public void ChangeGameOver( ParticleSystem lastExplode = null, Vector3 lastPosition = new Vector3()) 
+    {
+        if (lastExplode != null)
+        {
+            Instantiate(lastExplode, lastPosition, lastExplode.transform.rotation);
+            StartCoroutine(LastSeconds(lastExplode.main.duration));
+        }
+        _gameState = GameState.gameOver;
+        scoreText.gameObject.SetActive(false);
+        gameOverText.gameObject.SetActive(true);
+        resetButton.gameObject.SetActive(true);
+        if (lastExplode == null)
+        {
+            StartCoroutine(LastSeconds(0));
+        }
+    }
+
+    /// <summary>
+    /// Resetea el juego
+    /// </summary>
+    public void RestartGame() 
+    {
+        SceneManager.LoadScene(
+            SceneManager.GetActiveScene().name
+        );
+        Time.timeScale = 1;
+    }
+
+    /// <summary>
+    /// Espera el tiempo que dura la animacion x 2 para pausar el juego
+    /// </summary>
+    /// <param name="explosionTime">Tiempo de duracion de explosion</param>
+    /// <returns>Pausa el tiempo</returns>
+    IEnumerator LastSeconds(float explosionTime) 
+    {
+        yield return new WaitForSeconds(explosionTime * 2);
+        Time.timeScale = 0;
+    }
+}
---
+++ 09_ProyectoUdemy_QuickClink/Assets/_Scripts/ObjectController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    [SerializeField, Range(10f, 100f)]
+    private float maxInpulseForce = 16f;
+    [SerializeField, Range(10f, 100f)]
+    private float minImpulseForce = 5f;
+    [SerializeField, Range(0f, 20f)]
+    private float torqueForce = 15f;
+    private Rigidbody objectRb;
+    private GameManager gameManager;
+    [SerializeField]
+    private ParticleSystem explosionParticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        objectRb = GetComponent<Rigidbody>();
+        objectRb.AddForce(randomInpulseForce(), ForceMode.Impulse);
+        objectRb.AddTorque(randomTorqueForce(), ForceMode.Impulse);
+    }
+
+    /// <summary>
+    /// Genera una fuerza de impulso aleatoriamente en el eje Y
+    /// </summary>
+    /// <returns>Devuelve un Vector3 aleatoriamente</returns>
+    private Vector3 randomInpulseForce()
+    {
+        return Vector3.up * Random.Range(minImpulseForce, maxInpulseForce);
+    }
+
+    /// <summary>
+    /// Genera una fuerza de torque aleatoriamente
+    /// </summary>
+    /// <returns>Devuelve un Vector3 aletaroiamente</returns>
+    private Vector3 randomTorqueForce()
+    {
+        return new Vector3(Random.Range(-torqueForce, torqueForce),
+            Random.Range(-torqueForce, torqueForce),
+            Random.Range(-torqueForce, torqueForce)
+            );
+    }
+
+    private void Update()
+    {
+        if (this.transform.position.y <= -30f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bounds"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (gameManager.gameState.Equals(GameManager.GameState.inGame))
+            switch (this.gameObject.tag)
+            {
+                case "Food":
+                    Destroy(this.gameObject);
+                    gameManager.UpdateScore(1);
+                    break;
+                case "Bomb":
+                    Destroy(this.gameObject);
+                    gameManager.ChangeGameOver(explosionParticle, transform.position);
+                    break;
+                case "PowerUp":
+                    break;
+                case "RandomPowerUp":
+                    break;
+            }
+        InstatiateExplosion();
+    }
+
+    /// <summary>
+    /// Instancia la animacion de las particulas explotando
+    /// </summary>
+    void InstatiateExplosion()
+    {
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+    }
+}
