410054c90a27a2e50d986b98fcf42215ae4ba0ec
Jeronimo Repetto <jeronimorepetto@gmail.com>
2021-04-28 16:43:27 -0300

    Comienzo de ultimo proyecto del curso JhonLemon

---
+++ 10_ProyectoUdemy_Whack_A_Mole/Assets/Challenge 5/Scripts/DifficultyButtonX.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButtonX : MonoBehaviour
+{
+    private Button button;
+    private GameManagerX gameManagerX;
+    public int difficulty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManagerX = GameObject.Find("Game Manager").GetComponent<GameManagerX>();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    /* When a button is clicked, call the StartGame() method
+     * and pass it the difficulty value (1, 2, 3) from the button 
+    */
+    void SetDifficulty()
+    {
+        switch (button.gameObject.name)
+        {
+            case "Easy Button":
+                gameManagerX.StartGame(1);
+            break;
+            case "Medium Button":
+                gameManagerX.StartGame(2);
+                break;
+            case "Hard Button":
+                gameManagerX.StartGame(3);
+                break;
+        }
+    }
+
+
+
+}
---
+++ 10_ProyectoUdemy_Whack_A_Mole/Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManagerX : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI timeText;
+    public GameObject titleScreen;
+    public Button restartButton; 
+
+    public List<GameObject> targetPrefabs;
+
+    private int score;
+    private float spawnRate = 1.5f;
+    public bool isGameActive;
+    [SerializeField, Range(60, 280)]
+    private float time = 60;
+    private float spaceBetweenSquares = 2.5f; 
+    private float minValueX = -3.75f; //  x value of the center of the left-most square
+    private float minValueY = -3.75f; //  y value of the center of the bottom-most square
+
+    // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
+    public void StartGame(int num)
+    {
+        spawnRate /= num;
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        StartCoroutine(CounTime());
+        UpdateScore(score);
+        titleScreen.SetActive(false);
+        timeText.gameObject.SetActive(true);
+    }
+
+    // While game is active spawn a random target
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targetPrefabs.Count);
+            Instantiate(targetPrefabs[index], RandomSpawnPosition(), targetPrefabs[index].transform.rotation); 
+        }
+    }
+
+    // While game is active spawn a random target
+    IEnumerator CounTime()
+    {
+        while (isGameActive && time != 0)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            if (time == 1)
+            {
+                GameOver();
+            }
+            time--;
+            timeText.text = "Time: " + time.ToString();
+        }
+    }
+
+    // Generate a random spawn position based on a random index from 0 to 3
+    Vector3 RandomSpawnPosition()
+    {
+        float spawnPosX = minValueX + (RandomSquareIndex() * spaceBetweenSquares);
+        float spawnPosY = minValueY + (RandomSquareIndex() * spaceBetweenSquares);
+
+        Vector3 spawnPosition = new Vector3(spawnPosX, spawnPosY, 0);
+        return spawnPosition;
+
+    }
+
+    // Generates random square index from 0 to 3, which determines which square the target will appear in
+    int RandomSquareIndex()
+    {
+        return Random.Range(0, 4);
+    }
+
+    // Update score with value from target clicked
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        if (score < 0) 
+        {
+            GameOver();
+        }
+        scoreText.text = "score: " + score;
+    }
+
+    // Stop game, bring up game over text and restart button
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    // Restart game by reloading the scene
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
